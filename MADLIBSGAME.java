2223064bf7b55f291b411e5356a3e59ed25e8a65
P.SAISRISHANTH <srishanthparasa@gmail.com>
2025-08-09 15:02:25 +0530

    Create MADLIBSGAME.java

---
+++ MADLIBSGAME.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class MADLIBSGAME {
+    public static void main(String[] args) {
+        
+
+        Scanner scanner = new Scanner(System.in);
+
+        String adjective1;
+        String noun1;
+        String adjective2;
+        String verb1;
+        String adjective3;
+
+
+        System.out.println("Enter an adjective(description):");
+        adjective1 = scanner.nextLine();
+        System.out.println("Enter a noun(person, place, or thing):");
+        noun1 = scanner.nextLine();
+        System.out.println("Enter an adjective(description):");
+        adjective2 = scanner.nextLine();
+        System.out.println("Enter a verb end with -ing (action):");
+        verb1 = scanner.nextLine();
+        System.out.println("Enter an adjective(description):");
+        adjective3 = scanner.nextLine();
+
+
+
+
+        System.out.println("Today I went to a " + adjective1 + "zoo");
+        System.out.println("In an exhibition I saw a " + noun1 +".");
+        System.out.println(noun1 + " was " + adjective2 + " and " + verb1 + "!");
+        System.out.println("I was " + adjective3 + " !");
+
+
+
+
+        scanner.close();
+    }
+}
