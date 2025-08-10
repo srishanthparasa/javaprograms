be1c009e03517c5643884ebb33c218d38b3e4fbc
P.SAISRISHANTH <srishanthparasa@gmail.com>
2025-08-10 17:20:46 +0530

    Create SHOPPINGCART.java

---
+++ SHOPPINGCART.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+
+public class SHOPPINGCART {
+    public static void main(String[] args) {
+        //shopping cart program with items and its prices
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+        String item;
+        double price;
+        int quantity;
+        char currency = '$';
+        double total;
+
+
+
+        System.out.println("What would you like to buy?: ");
+        item = scanner.nextLine();
+
+        System.out.println("What is the price for each?: ");
+        price = scanner.nextDouble();
+
+        System.out.println("How many do you want?: ");
+        quantity = scanner.nextInt();
+
+        total = price * quantity;
+
+        System.out.println("\nYou have bought" + quantity + " " + item + "/s");
+        System.out.println("Your total is " + currency + total);
+
+        scanner.close();
+
+        
+    }
+    
+}
